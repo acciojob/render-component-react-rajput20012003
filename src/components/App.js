8cfc5256bff0fe02f3875e9aa2c172785be12394
VaishnaviRajput <rajputvaishnavikaransing10@gmail.com>
2024-06-24 21:08:29 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import './App.css';
+import ReactDOM from 'react-dom';
 
 const App = () => {
   return (
-    <div id="main">
-      <h1>Welcome to My Learning Journey</h1>
+    <div id="root">
+      <h1>Welcome to React</h1>
       <p>I am learning React. My life is getting better.</p>
     </div>
   );
-}
+};
 
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
