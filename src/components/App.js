2305b0b84a6768057177d2b588b0017e64867e77
VaishnaviRajput <rajputvaishnavikaransing10@gmail.com>
2024-06-24 20:44:35 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from 'react';
+import './App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
+    <div id="main">
+      <h1>Welcome to My Learning Journey</h1>
+      <p>I am learning React. My life is getting better.</p>
+    </div>
+  );
 }
-
 
 export default App;
